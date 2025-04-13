3bab323c2fc545c215c881e090fb48943652c82c
gecec <gael1142@gmail.com>
2025-04-13 13:48:24 +0300

    check if MOM folder exists, if not create it. check if MOM file exists, if yes do not generate one

--- main.ts
+++ main.ts
@@ -42,44 +42,6 @@ export default class HelloWorldPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText("Status Bar Text");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
@@ -95,11 +57,17 @@ export default class HelloWorldPlugin extends Plugin {
 		);
 
 		this.addCommand({
-			id: "convert-to-uppercase",
-			name: "Convert to uppercase",
+			id: "generate-mom",
+			name: "Generate MOM for selected meetings",
 			editorCallback: (editor: Editor) => {
 				const selection = editor.getSelection();
 				const tasks = selection.split("\n");
+				const vault = this.app.vault;
+				// check if folder exists
+				const momFolder = vault.getFolderByPath("MOM");
+				if (momFolder == null) {
+					vault.createFolder("MOM");
+				}
 
 				for (const idx in tasks) {
 					const newTask = tasks[idx].replace("- [ ] ", "");
@@ -117,15 +85,18 @@ export default class HelloWorldPlugin extends Plugin {
 
 					const dateString =
 						today.getFullYear() + "-" + month + "-" + day;
-					// check if file already exists or not
-					// check if folder exists
-					this.app.vault.create(
-						"MOM/" + dateString + "-" + newTask + ".md",
-						"qwerty",
-					);
-				}
 
-				// editor.replaceSelection(selection.toUpperCase());
+					const filePath =
+						"MOM/" + dateString + "-" + newTask + ".md";
+
+					// check if file already exists or not
+					const file = vault.getFileByPath(filePath);
+					if (file == null) {
+						vault.create(filePath, "");
+					} else {
+						console.warn("File already exists", filePath);
+					}
+				}
 			},
 		});
 	}
